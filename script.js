d8787fc29ef67637c46e8a326c103970d43350cb
Shubham <sm075174@gmail.com>
2024-02-27 00:55:40 +0530

    navigation complete with little with faded animation

---
+++ script.js
@@ -0,0 +1,8 @@
+function navAnimation() {
+  document.addEventListener("DOMContentLoaded", function (event) {
+    // Add fade-in class to the navigation on page load
+    document.querySelector("nav").classList.add("fade-in");
+  });
+}
+
+navAnimation();
