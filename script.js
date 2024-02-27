f189bdcf8a011ec4e0f9e200c25bd79c7345b846
Shubham <sm075174@gmail.com>
2024-02-28 00:12:00 +0530

    Filterable Project section done

--- script.js
+++ script.js
@@ -6,54 +6,92 @@ function navAnimation() {
   });
 }
 
+// Project Animation
+
+function projectsAnimation() {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".projects",
+      // markers: true,
+      start: "50% 50%",
+      end: "100% 50%",
+      scrub: 2,
+      pin: true,
+    },
+  });
+
+  tl.to(
+    ".top-project",
+    {
+      top: "-50%",
+    },
+    "a"
+  );
+  tl.to(
+    ".bottom-project",
+    {
+      bottom: "-50%",
+    },
+    "a"
+  );
+
+  tl.to(
+    ".top-h",
+    {
+      top: "80%",
+    },
+    "a"
+  );
+
+  tl.to(
+    ".bottom-h",
+    {
+      bottom: "80%",
+    },
+    "a"
+  );
+
+  tl.to(
+    ".content",
+    {
+      marginTop: "0%",
+    },
+    "a"
+  );
+}
+
+// Filterable Project
+
+function filterablePro() {
+  const filterableButtons = document.querySelectorAll(".fltr-btn button");
+  const filterableCards = document.querySelectorAll(".fltr-cards .card");
+
+  // Define the filterCards Function
+
+  const filterCards = (e) => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+
+    // Iterate over each filterable card
+    filterableCards.forEach((card) => {
+      // Add "hide" class to hide the card initialy
+      card.classList.add("hide");
+      // Check if the card matches the selected filters or "all" is selected
+      if (
+        card.dataset.name === e.target.dataset.name ||
+        e.target.dataset.name == "all"
+      ) {
+        card.classList.remove("hide");
+      }
+    });
+  };
+  // Add Click event listener to each filter button
+
+  filterableButtons.forEach((button) =>
+    button.addEventListener("click", filterCards)
+  );
+}
+
 navAnimation();
-
-let tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".projects",
-    // markers: true,
-    start: "50% 50%",
-    end: "100% 50%",
-    scrub: 2,
-    pin: true,
-  },
-});
-
-tl.to(
-  ".top-project",
-  {
-    top: "-50%",
-  },
-  "a"
-);
-tl.to(
-  ".bottom-project",
-  {
-    bottom: "-50%",
-  },
-  "a"
-);
-
-tl.to(
-  ".top-h",
-  {
-    top: "80%",
-  },
-  "a"
-);
-
-tl.to(
-  ".bottom-h",
-  {
-    bottom: "80%",
-  },
-  "a"
-);
-
-tl.to(
-  ".content",
-  {
-    marginTop: "0%",
-  },
-  "a"
-);
+projectsAnimation();
+filterablePro();
