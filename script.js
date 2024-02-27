a0b622105fa472e70f7e677d653af7eee4a8b5d8
Shubham <sm075174@gmail.com>
2024-02-27 22:42:48 +0530

    project section  animation done

--- script.js
+++ script.js
@@ -7,3 +7,53 @@ function navAnimation() {
 }
 
 navAnimation();
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".projects",
+    // markers: true,
+    start: "50% 50%",
+    end: "100% 50%",
+    scrub: 2,
+    pin: true,
+  },
+});
+
+tl.to(
+  ".top-project",
+  {
+    top: "-50%",
+  },
+  "a"
+);
+tl.to(
+  ".bottom-project",
+  {
+    bottom: "-50%",
+  },
+  "a"
+);
+
+tl.to(
+  ".top-h",
+  {
+    top: "80%",
+  },
+  "a"
+);
+
+tl.to(
+  ".bottom-h",
+  {
+    bottom: "80%",
+  },
+  "a"
+);
+
+tl.to(
+  ".content",
+  {
+    marginTop: "0%",
+  },
+  "a"
+);
