5134685869ac4ea26380fd018c42f6c2f1faf90c
Shubham <sm075174@gmail.com>
2024-03-01 18:04:18 +0530

    hamburger issue solve its not showing under 400 px width screen

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // NAV Animation fade
 function navAnimation() {
-  document.addEventListener("DOMContentLoaded", function (event) {
-    // Add fade-in class to the navigation on page load
-    document.querySelector("nav").classList.add("fade-in");
-  });
+  // Add fade-in class to the navigation on page load
+  document.querySelector(".navigation").classList.add("fade-in");
 }
 
 // Project Animation
